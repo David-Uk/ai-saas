635f93062c7d51c1eba7b198ee89dfb2fdc27af4
David Ukelere <david.ukelere291@gmail.com>
2023-09-18 03:29:51 +0100

    Started sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs"
 
 export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
